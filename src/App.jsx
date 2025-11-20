c7c32e0413aff98e9cf4a5b29c9a5d8003ac098b
AnushkaShettigar <anushka.shettigar.1706@gmail.com>
2025-11-20 19:53:02 +0530

    um

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './sections/navbar'
+
+const App = () => {
+  return (
+    <div className="container mx-auto mx-w-7xl">
+        <Navbar />
+    {/* // hero
+    // about
+    // project
+    // education
+    // experience
+    // testimonials
+    // contact
+    // footer */}
+    
+    </div>
+  )
+}
+
+export default App
